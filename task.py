d64163bb27f96b5f35bf6a74d1ff68d360073637
Szymon Łondka <szymon.londka@gmail.com>
2023-02-20 21:13:52 +0100

    Change: completion of the task

--- task.py
+++ task.py
@@ -1,27 +1,36 @@
-#!/usr/bin/env python
+'''
+Zadanie 1
+Pamiętasz zadanie z listą zakupów?
 
-import subprocess, smtplib, re
+Załóżmy, że mamy do zrobienia zakupy spożywcze, potrzebujemy pójść do piekarni, w której kupujemy chleb, bułki oraz pączka. Poza tym po drodze wstąpimy też do warzywniaka, gdzie kupimy marchew, seler i rukolę.
 
+W pliku, który właśnie utworzyliśmy:
 
-def send_mail(email, password, message):
-    server = smtplib.SMTP("smtp.gmail.com", 587)
-    server.starttls()
-    server.login(email, password)
-    server.sendmail(email, email, message)
-    server.quit()
+zdefiniuj słownik zawierający listę zakupów, gdzie kluczem jest nazwa sklepu, a wartością lista przedmiotów, które chcesz kupić w danym sklepie.
+Następnie za pomocą pętli for, przeiteruj po tym słowniku i wyświetl napis w postaci: Idę do <sklep> i kupuję tam <rzeczy>.
+Dodatkowo, używając wbudowanych metod operacji na napisach, spowoduj, aby nazwy sklepów i towarów były wypisane wielką literą (sięgnij do oficjalnej dokumentacji, by odnaleźć taką funkcjonalność).
+Na koniec, w ostatniej linii wypisz W sumie kupuję <X> produktów.. X to sumaryczna liczba towarów, które są na listach.
+Twój program po uruchomieniu, powinien wyświetlić następujące informacje:
+'''
 
+shops = {"piekarni":("chleb", "bułki", "pączki"), "warzywniaka":("marchew", "seler", "rukola")}
+x = 0
 
-command = "netsh wlan show profile"
+print("Lista Zakupów")
+for shop in shops:
+    print("Ide do " + shop.capitalize() +", kupuję tam: " + str(shops[shop]))
+    x += len(shops[shop])
 
-networks = subprocess.check_output(command, shell=True)
-networks_name_list = re.findall("(?:Profile\s*:\s)(.*)", str(networks))
+print(x)
 
-print(networks_name_list)
+'''
+Zadanie 2
+Napisz program, który:
 
-result = ""
-for network_name in networks_name_list:
-    command = "netsh wlan show profile " + network_name + " key=clear"
-    current_result = subprocess.check_output(command, shell=True)
-    result = result + current_result
+Dla liczb z zakresu od 0 do 100, wyświetli te, które są podzielne przez 5.
+W następnym wierszu wyświetli te liczby podniesione do potęgi 3.
+'''
 
-send_mail("szymon.londka@gmail.com", "tufxtmigajgjxqsv", result)
+for x in range(0,101):
+    if x % 5 == 0:
+        print(x**3)
