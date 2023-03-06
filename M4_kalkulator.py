935c678bf7cf858a008a54aed3538788435ce482
Szymon Łondka <szymon.londka@gmail.com>
2023-03-06 14:12:46 +0100

    M4 Kalkulator fix nr.2

--- M4_kalkulator.py
+++ M4_kalkulator.py
@@ -8,17 +8,35 @@ math_string = ""
 
 try:
     calculation_type = int(input("Podaj działanie, posługując się odpowiednią liczbą: 1 Dodawanie, 2 Odejmowanie, 3 Mnożenie, 4 Dzielenie: "))
-    if calculation_type == 1 or calculation_type == 3:
-        how_many_numbers = int(input("Z ilu liczb będzie składać się to działanie? "))
-        for x in range(how_many_numbers):
-            y = int(input("Podaj wartośc nr."+str(x+1)+ " "))
-            values.append(y)
-    else:
-        for x in range(how_many_numbers):
-            y = int(input("Podaj wartośc nr."+str(x+1)+ " "))
-            values.append(y)
+    if calculation_type > 4:
+        print("Podałeś nieodpowiednią liczbę")
+        exit(1)
 except ValueError:
-    print("Podaj poprawne wartości liczbowe, aby wykonać działanie")
+    print("Podaj liczbę do określenia rodzaju działania")
+
+
+if calculation_type == 1 or calculation_type == 3:
+    try:
+        how_many_numbers = int(input("Z ilu liczb będzie składać się to działanie? "))
+    except ValueError:
+        print("Podano nieodpowiednią liczbę wartości")
+        exit(1)
+
+    for x in range(how_many_numbers):
+        try:
+            y = int(input("Podaj wartośc nr."+str(x+1)+ " "))
+            values.append(y)
+        except ValueError:
+            print("Podano nieodpowiednią wartość")
+            exit(1)
+else:
+    for x in range(how_many_numbers):
+        try:
+            y = int(input("Podaj wartośc nr."+str(x+1)+ " "))
+            values.append(y)
+        except ValueError:
+            print("Podano nieodpowiednią wartość")
+            exit(1)
 
 for number in values:
     math_string += str(number) + " " + calculation_type_dict[calculation_type] +" "
